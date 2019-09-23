e6f4f895712363b1726d0a60db1cb8eab293f789
Max Anselmo <itmanselmo@gmail.com>
2019-09-23 14:37:59 -0400

    implemented newly learned way to programmatically create pages through queries, specifially through using markdown files and importing their data to the new pages.

--- gatsby-site/src/pages/index.js
+++ gatsby-site/src/pages/index.js
@@ -1,9 +1,8 @@
 import React from "react"
-import { graphql } from "gatsby"
 import { css } from "@emotion/core"
+import { Link, graphql } from "gatsby"
 import { rhythm } from "../utils/typography"
 import Layout from "../components/layout"
-
 export default ({ data }) => {
   console.log(data)
   return (
@@ -15,11 +14,18 @@ export default ({ data }) => {
             border-bottom: 1px solid;
           `}
         >
-          Amazing Pandas Eating Things
+          Check out some of my finished projects!
         </h1>
         <h4>{data.allMarkdownRemark.totalCount} Posts</h4>
         {data.allMarkdownRemark.edges.map(({ node }) => (
           <div key={node.id}>
+            <Link 
+              to={node.fields.slug}
+              css={css`
+              text-decoration: none; 
+              color: inherit;
+              `}
+            >
             <h3
               css={css`
                 margin-bottom: ${rhythm(1 / 4)};
@@ -35,16 +41,16 @@ export default ({ data }) => {
               </span>
             </h3>
             <p>{node.excerpt}</p>
+              </Link>
           </div>
         ))}
       </div>
     </Layout>
   )
 }
-
 export const query = graphql`
   query {
-    allMarkdownRemark {
+    allMarkdownRemark(sort: { fields: [frontmatter___date], order: DESC }) {
       totalCount
       edges {
         node {
@@ -52,6 +58,9 @@ export const query = graphql`
           frontmatter {
             title
             date
+          }
+          fields {
+            slug
           }
           excerpt
         }
---
+++ gatsby-site/src/pages/test2.md
@@ -0,0 +1,5 @@
+---
+title: "This is another test"
+date: "9-20-2019"
+---
+heres another test i did
