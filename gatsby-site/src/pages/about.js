fa554228993c151e486f561e0c441f67fca1d32f
Max Anselmo <itmanselmo@gmail.com>
2019-10-24 14:45:56 -0400

    Added to about page and modified contact page.

--- gatsby-site/src/pages/about.js
+++ gatsby-site/src/pages/about.js
@@ -3,6 +3,8 @@ import Layout from "../components/layout"
 export default () => (
     <Layout>
         <h1>About me</h1>
-        <p>I'm good enough, I'm smart enough, and people love me!</p>
+        <p>Developing developer, pursuing an Associates in Computer Science at Suffolk County Community College,
+            and trying to save the world one bit at a time. 
+        </p>
     </Layout>
 ) 
--- gatsby-site/src/pages/contact.js
+++ gatsby-site/src/pages/contact.js
@@ -3,7 +3,7 @@ import { css } from "@emotion/core"
 import Layout from "../components/layout"
 export default () => (
     <Layout>
-        <h1>I'd love to talk! Email me at the address below</h1>
+        <h1>I'd love to talk! Email me at the address below:</h1>
         <p css={css`
             text-align: center;
             `}>
